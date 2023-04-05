309d0ff0d2aed7f7408251533aeadf5a8628120e
Eunsol Kim <eunskim@student.42heilbronn.de>
2023-04-05 21:07:08 +0200

    added a data struct and wait function

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: eunskim <eunskim@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 20:31:10 by eunskim           #+#    #+#             */
-/*   Updated: 2023/04/04 20:15:39 by eunskim          ###   ########.fr       */
+/*   Updated: 2023/04/05 20:24:20 by eunskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,14 @@
 # include <fcntl.h>
 # include "libft.h"
 # include "get_next_line.h"
+
+typedef struct s_data
+{
+	int		pipe_fds[2];
+	int		infile_fd;
+	int		outfile_fd;
+	pid_t	ret_pid1;
+	pid_t	ret_pid2;
+}	t_data;
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: eunskim <eunskim@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 18:30:37 by eunskim           #+#    #+#             */
-/*   Updated: 2023/04/04 20:36:08 by eunskim          ###   ########.fr       */
+/*   Updated: 2023/04/05 21:01:17 by eunskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,67 +19,79 @@
 // By overwriting it will not change the permission.
 // When needed permission is not given, an error message will be printed.
 
-void	second_child_process(char *outfile, char *cmd2, char **envp, int *pipe_fds)
+void	second_child_process(char *outfile, char *cmd2, char **envp, t_data *pipex)
 {
-	int	outfile_fd;
-
-	outfile_fd = open(outfile, O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (outfile_fd == -1)
-		error();
-	if (close(pipe_fds[1]) == -1)
-		error();
-	dup2(pipe_fds[0], STDIN_FILENO);
+	pipex->outfile_fd = open(outfile, O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (pipex->outfile_fd == -1)
+		error("File open error");
+	if (close(pipex->pipe_fds[1]) == -1)
+		error("Closing pipe end failed");
+	dup2(pipex->pipe_fds[0], STDIN_FILENO);
 }
 
-void	first_child_process(char *infile, char	*cmd1, char **envp, int *pipe_fds)
+int	first_child_process(char *infile, char *cmd1, char **envp, t_data *pipex)
 {
-	int	infile_fd;
-
-	infile_fd = open(infile, O_RDONLY);
-	if (infile_fd == -1)
-		error_not_exiting();
-	if (close(pipe_fds[0]) == -1)
-		error_not_exiting();
-	dup2(pipe_fds[1], STDOUT_FILENO);
+	pipex->infile_fd = open(infile, O_RDONLY);
+	if (pipex->infile_fd == -1)
+	{
+		perror("File open error");
+		return (1);
+	}
+	if (close(pipex->pipe_fds[0]) == -1)
+	{
+		perror("Closing pipe end failed");
+		return (1);
+	}
+	dup2(pipex->pipe_fds[1], STDOUT_FILENO);
 }
 
-void	error_not_exiting(void)
+void	wait_and_error_exit(t_data pipex)
 {
-	perror("Error");
+	int	wstatus1;
+	int	wstatus2;
+	int	status_code;
+
+	waitpid(pipex.ret_pid1, &(wstatus1), 0);
+	waitpid(pipex.ret_pid2, &(wstatus2), 0);
+	if (WIFEXITED(wstatus2))
+		status_code = WEXITSTATUS(wstatus2);
+	if (status_code != 0)
+	{
+		errno = status_code;
+		error(NULL);
+	}
 	return ;
 }
 
-void	error(void)
+void	error(char *err_msg)
 {
-	perror("Error");
+	perror(err_msg);
 	exit(EXIT_FAILURE);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	int		pipe_fds[2];
-	pid_t	ret_pid;
+	t_data	pipex;
 
 	if (argc != 5)
 	{
-		printf("Invalid arguments. Usage: ./pipex file1 cmd1 cmd2 file2");
+		ft_printf("Invalid arguments. Usage: ./pipex file1 cmd1 cmd2 file2");
 		exit(EXIT_FAILURE);
 	}
-	if (pipe(pipe_fds) == -1)
-		error();
-	ret_pid = fork();
-	if (ret_pid == -1)
-		error();
-	if (ret_pid == 0)
-		first_child_process(argv[1], argv[2], envp, pipe_fds);
-	ret_pid = fork();
-	if (ret_pid == -1)
-		error();
-	if (ret_pid == 0)
-		second_child_process(argv[4], argv[3], envp, pipe_fds);
-	if (close(pipe_fds[0]) == -1 || close(pipe_fds[1] == -1))
-		error();
-	wait(NULL);
-	wait(NULL);
+	if (pipe(pipex.pipe_fds) == -1)
+		error("Pipe function failed");
+	pipex.ret_pid1 = fork();
+	if (pipex.ret_pid1 == -1)
+		error("Failed to create first child process");
+	if (pipex.ret_pid1 == 0)
+		first_child_process(argv[1], argv[2], envp, &pipex);
+	pipex.ret_pid2 = fork();
+	if (pipex.ret_pid2 == -1)
+		error("Failed to create second child process");
+	if (pipex.ret_pid2 == 0)
+		second_child_process(argv[4], argv[3], envp, &pipex);
+	if (close(pipex.pipe_fds[0]) == -1 || close(pipex.pipe_fds[1] == -1))
+		error("Closing pipe end failed");
+	wait_and_error_exit(pipex);
 	exit(EXIT_SUCCESS);
 }
